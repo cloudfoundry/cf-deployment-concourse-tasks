7b9e3a729a969635cea65be890cccafc3362d7d7
Jesse Alford <jalford@pivotal.io>
2017-01-12 18:09:36 -0800

    Add a task to upload stemcell
    - This was brought in with minor modifications from
    cloudfoundry/runtime-ci
    
    [#135554221]

---
+++ bosh-upload-stemcell/task.bash
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -eux
+
+STEMCELLS_URL="https://bosh.io/d/stemcells"
+
+BOSH_AGENT="go_agent"
+OS=$(bosh interpolate --path=/stemcells/alias=default/os cf-deployment/cf-deployment.yml)
+VERSION=$(bosh interpolate --path=/stemcells/alias=default/version cf-deployment/cf-deployment.yml)
+
+BOSH_ENVIRONMENT=$(cat "target/${TARGET_FILE}")  && export BOSH_ENVIRONMENT
+BOSH_USER=$(cat username/"${USERNAME_FILE}")     && export BOSH_USER
+set +x
+BOSH_CA_CERT="ca-cert/${CA_CERT_FILE}"           && export BOSH_CA_CERT
+BOSH_PASSWORD=$(cat "password/${PASSWORD_FILE}") && export BOSH_PASSWORD
+set -x
+
+# Ask bosh if it already has our OS / version stemcell combination
+# As of this writing, the new bosh cli doesn't have --skip-if-exists
+
+set +e
+EXISTING_STEMCELL=$(bosh stemcells | grep "$OS" | awk '{print $2}' | tr -d "\*" | grep ^"$VERSION"$ )
+set -e
+
+STEMCELL_NAME="bosh"
+
+if [ "$INFRASTRUCTURE" = "aws" ]; then
+  STEMCELL_NAME="$STEMCELL_NAME-aws-xen-hvm"
+elif [ "$INFRASTRUCTURE" = "google" ]; then
+  STEMCELL_NAME="$STEMCELL_NAME-google-kvm"
+elif [ "$INFRASTRUCTURE" = "boshlite" ]; then
+  STEMCELL_NAME="$STEMCELL_NAME-warden-boshlite"
+elif [ "$INFRASTRUCTURE" = "bosh-lite" ]; then
+  STEMCELL_NAME="$STEMCELL_NAME-warden-boshlite"
+elif [ "$INFRASTRUCTURE" = "vsphere" ]; then
+  STEMCELL_NAME="$STEMCELL_NAME-vsphere-esxi"
+fi
+
+STEMCELL_NAME="$STEMCELL_NAME-$OS-$BOSH_AGENT"
+FULL_STEMCELL_URL="$STEMCELLS_URL/$STEMCELL_NAME?v=$VERSION"
+
+# If bosh already has our stemcell, exit 0
+if [ "$EXISTING_STEMCELL" ]; then
+  echo "Task bosh-upload-stemcell:"
+  echo "Stemcell '$STEMCELL_NAME/$VERSION' already exists.  Exiting..."
+  exit 0
+fi
+
+# ... otherwise, begin the upload process
+set +x
+bosh \
+  -n \
+  upload-stemcell \
+  "$FULL_STEMCELL_URL"
+set -x
+
