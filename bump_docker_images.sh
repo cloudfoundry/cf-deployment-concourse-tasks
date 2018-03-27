0afe2104ce95917ac3b5854619c187dd05b09c0a
David Sabeti <dsabeti@pivotal.io>
2018-03-27 16:08:54 -0700

    Check in script for bumping docker images

---
+++ bump_docker_images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OLD_VERSION="${1?: Please define an old version}"
+NEW_VERSION="${2?: Please define a new version}"
+
+MAJOR_VERSION="$(echo "$NEW_VERSION" | cut -d'.' -f1)"
+
+docker tag relintdockerhubpushbot/cf-deployment-concourse-tasks:latest relintdockerhubpushbot/cf-deployment-concourse-tasks:$NEW_VERSION
+docker tag relintdockerhubpushbot/cf-deployment-concourse-tasks:latest relintdockerhubpushbot/cf-deployment-concourse-tasks:$MAJOR_VERSION
+
+docker push relintdockerhubpushbot/cf-deployment-concourse-tasks:$NEW_VERSION
+docker push relintdockerhubpushbot/cf-deployment-concourse-tasks:$MAJOR_VERSION
+
+perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" **/task.yml
+perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" credhub-compatible/**/task.yml
+git add */task.yml
